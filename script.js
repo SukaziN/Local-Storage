fd4af4a6171ff5abac026547368e0aa3761c96e3
SukaziN <nkosiyazisukazie@gmail.com>
2026-05-29 09:48:17 +0200

    defined an empty array and pushed user input into it

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-let userInput = document.getElementById("input");
 let button = document.getElementById("button");
 let output = document.getElementById("output");
+let tasks = [];
+
+function addTask() {
+  let userInput = document.getElementById("input").value;
+  tasks.push(userInput);
+  console.log(tasks);
+}
+button.addEventListener("click", addTask);
