90a39695f7585d29f4f56f5db4db738eeb2aaa39
SukaziN <nkosiyazisukazie@gmail.com>
2026-06-08 12:31:56 +0200

    practiced local storage

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+localStorage.setItem("username", "JaneDoe");
+let username = localStorage.getItem("username");
+console.log(username);
+
 let button = document.getElementById("button");
 let output = document.getElementById("output");
 let tasks = [];
 
 function addTask() {
   let userInput = document.getElementById("input").value;
-  tasks.push(userInput);
-  console.log(tasks);
-  output.innerHTML = tasks;
+  localStorage.setItem("list", userInput);
+  userInput = localStorage.getItem("list");
+  if (localStorage.getItem("list")) {
+    document.getElementById("output").innerHTML = localStorage.getItem("list");
+  } else {
+    console.log("I don't remember that!");
+  }
 }
 button.addEventListener("click", addTask);
-
-function saveTask() {}
